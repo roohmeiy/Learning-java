7aba2b9ad012f16973768c5189796c85067ec135
Payal Sharma <roohmeiy@gmail.com>
2025-04-09 00:34:51 +0530

    Delete src/com/anudip/learning directory

--- src/com/anudip/learning/Student.java
+++
@@ -1,9 +0,0 @@
-package com.anudip.learning;
-
-class Student {
-    private String studentName, collegeName;
-    private int studentID;
-    public static void main(String[] args) {
-        System.out.println("Successful");
-    }
-}
